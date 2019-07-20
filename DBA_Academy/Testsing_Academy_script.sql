52f9dc65710a07092e9c4a6c0d0bcc7fd0be48a0
Garavirod <rodrigogarciaavila26@gmail.com>
2019-07-20 01:37:48 -0500

    Testing DBA Aacademy II

--- DBA_Academy/Testsing_Academy_script.sql
+++ DBA_Academy/Testsing_Academy_script.sql
@@ -107,7 +107,135 @@ GO
 INSERT INTO States (code_St,cod_country,nam) VALUES('SW','GB','South West England')
 GO
 
---So
-
+--So, first we have to insert the country
 INSERT INTO Countries (cod_country,nam,code_ISO3,node_tel) VALUES('GB','Gran Bretaña','BRN',89)
+GO
+
+INSERT INTO States(code_St,cod_country,nam) VALUES('SW','GB','South West England')
+
+--Other states 
+
+INSERT INTO Countries (cod_country,nam,code_ISO3,node_tel) VALUES('US','United States','USA',1)
+INSERT INTO Countries (cod_country,nam,code_ISO3,node_tel) VALUES('WI','Wisconsing','WIS',3)
+INSERT INTO Countries (cod_country,nam,code_ISO3,node_tel) VALUES('CF','California','CAL',9)
+GO
+
+INSERT INTO States(code_St,cod_country,nam) VALUES('WI','US','Wisconsing')
+INSERT INTO States(code_St,cod_country,nam) VALUES('CF','US','California')
+GO
+
+--Inserting another states Carabobo in Venezuela
+INSERT INTO Countries (cod_country,nam,code_ISO3,node_tel) VALUES('VE','Venezuela','VEN',53)
+INSERT INTO States (code_St,cod_country,nam) VALUES('V','VE','Carabobo')
+GO
+
+/*
+	Error:
+		cod_St char(2) PRIMARY KEY CHECK(LEN(cod_St)=2)
+		Let's delete the restriction
+*/
+ALTER TABLE States DROP CONSTRAINT Len_State
+GO
+
+--let's create a new Restriction
+ALTER TABLE States
+ADD CONSTRAINT Len_State CHECK (LEN(code_St)>0)
+GO
+
+
+--Let's insert again
+INSERT INTO States (code_St,cod_country,nam) VALUES('Y','VE','Carabobo')
+GO
+
+--Let's see the record Venezuela and Carabobo state that we've just created 
+SELECT * FROM States WHERE cod_country = 'VE'
+SELECT * FROM Countries WHERE cod_country = 'VE'
+GO
+
+--Let's testing delete cascade and update cascade
+
+--Update Venezuela Code
+UPDATE Countries --Modifying Countries Table
+SET cod_country = 'XX' --Set 'XX' in cod_cpountry field
+WHERE cod_country = 'VE'
+GO
+
+--Let's see the changes
+SELECT * FROM Countries WHERE cod_country = 'XX'
+SELECT * FROM States WHERE code_St = 'Y'
+GO
+
+--Le's deleteing cascade
+DELETE Countries WHERE	cod_country = 'XX'
+SELECT * FROM States
+GO
+
+
+/*Testing Academies */
+--We can't asgin a value, there're esceptions
+INSERT INTO Academies (code_Ac,Nam,Date_Func,Num,Street,City,Sta,Zip)
+VALUES(
+	1,
+	'Relational Algebar Academy "Edgar F. Cod"',
+	'20150301','13528',
+	'Avenida Datum',
+	'Relational City',
+	'WI',
+	'12345')
+GO
+
+INSERT INTO Academies (Nam,Date_Func,Num,Street,City,Sta,Zip)
+VALUES(
+	'Relational Algebar Academy "Edgar F. Cod"',
+	'20150301',
+	'13528',
+	'Avenida Datum',
+	'Relational City',
+	'WI',
+	'12345')
+GO
+
+SELECT * FROM Academies
+GO
+
+--Modifying a table's atribute
+ALTER TABLE dbo.Academies ALTER COLUMN Nam VARCHAR(50) NOT NULL
+
+INSERT INTO Academies (Nam,Date_Func,Num,Street,City,Sta,Zip)
+VALUES(
+	'Einstein Academy',
+	'20181217',
+	'5582',
+	'Relation Street',
+	'Ulm',
+	'UL',
+	'00000')
+GO
+
+/*
+	To avoid posible generated conflicts for concurrency
+	the value of fields IDENTITY it incresses itself before executing the insertition
+	and got value  can't be used
+*/
+
+--It Allows inserting data manualy in the Field IDENTITY
+SET IDENTITY_INSERT Academies ON
+GO
+
+--You must set explict in the Insertion list field
+INSERT INTO Academies (code_Ac,Nam,Date_Func,Num,Street,City,Sta,Zip)
+VALUES(
+	2,
+	'Academy 3FN',
+	'20160301',
+	'4675',
+	'Tuple Avenue Union',
+	'California',
+	'CF',
+	'12345')
+GO
+
+
+--It's very import return back normal behavor
+SET IDENTITY_INSERT Academies OFF
 GO
